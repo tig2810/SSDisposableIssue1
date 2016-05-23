3fcada1c09086e4ea06e2866e3010d16b4e3b9f0
HOME <HOME@DESKTOP-CVK8FTA>
2016-05-23 11:08:40 +0100

    Add project files.

---
+++ SSDisposableIssue/Global.asax.cs
@@ -0,0 +1,101 @@
+﻿using Funq;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace SSDisposableIssue
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class TestDisposible : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+        /// <summary>
+        /// Create your ServiceStack web service application with a singleton AppHost.
+        /// </summary>        
+        public class HelloAppHost : AppHostBase
+        {
+            /// <summary>
+            /// Initializes a new instance of your ServiceStack application, with the specified name and assembly containing the services.
+            /// </summary>
+            public HelloAppHost() : base("Hello Web Services", typeof(HelloService).Assembly) { }
+
+            /// <summary>
+            /// Configure the container with the necessary routes for your ServiceStack application.
+            /// </summary>
+            /// <param name="container">The built-in IoC used with ServiceStack.</param>
+            public override void Configure(Container container)
+            {
+                //Register user-defined REST-ful urls. You can access the service at the url similar to the following.
+                //http://localhost/ServiceStack.Hello/servicestack/hello or http://localhost/ServiceStack.Hello/servicestack/hello/John%20Doe
+                //You can change /servicestack/ to a custom path in the web.config.
+                Routes
+                  .Add<Hello>("/hello")
+                  .Add<Hello>("/hello/{Name}");
+
+                container.RegisterAutoWired<TestDisposible>().ReusedWithin(ReuseScope.None);
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            (new HelloAppHost()).Init();
+
+            new Thread(new ThreadStart(() =>
+            {
+                // Following test pass
+                Test();
+                
+                var d = HelloAppHost.Instance.Container.Resolve<TestDisposible>();
+
+                // Folloing execution fails
+                Test();
+            })).Start();
+        }
+
+        private void Test()
+        {
+            var domain = AppDomain.CreateDomain("Test");
+            var list = domain.GetAssemblies();
+            AppDomain.Unload(domain);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SSDisposableIssue/HelloService.cs
@@ -0,0 +1,37 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSDisposableIssue
+{
+    /// <summary>
+    /// Create your ServiceStack web service implementation.
+    /// </summary>
+    public class HelloService : IService
+    {
+        public object Any(Hello request)
+        {
+            //Looks strange when the name is null so we replace with a generic name.
+            var name = request.Name ?? "John Doe";
+            return new HelloResponse { Result = "Hello, " + name };
+        }
+    }
+
+    /// <summary>
+    /// Define your ServiceStack web service response (i.e. Response DTO).
+    /// </summary>
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    /// <summary>
+    /// Define your ServiceStack web service request (i.e. the Request DTO).
+    /// </summary>    
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+}
